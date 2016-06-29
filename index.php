dde2813bb08ccf5442a3901f04bdb98889d9adde
andrev11 <andrevalerian@yahoo.com>
2016-06-29 14:40:48 +0700

    add bg

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<style>
+        body {
+            background-image: url("bg.jpg");
+            background-repeat: no-repeat;
+            background-size: cover;
+            /*opacity: 0.9;*/
+        }
+</style>
+
 <?php 
 
 if( !session_id() )
@@ -13,11 +22,13 @@ if(@$_SESSION['logged_in'] == true){
 <script type="text/javascript" src="./main.js"></script>
 <script type="text/javascript" src="./llqrcode.js"></script>
 
+<body>
 <div style="display:none" id="result"></div>
 	<div class="selector" id="webcamimg" onclick="setwebcam()" align="left" ></div>
 		<div class="selector" id="qrimg" onclick="setimg()" align="right" ></div>
 			<center id="mainbody"><div id="outdiv"></div></center>
 				<canvas id="qr-canvas" width="800" height="600"></canvas>
+</body>
 
 <script type="text/javascript">load();</script>
 <script src="./jquery-1.11.2.min.js"></script>
